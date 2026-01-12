5f42dd352f754f36c00b8a9bad5e7278e7681fe2
Abdelrhman Sayed <abdulrhmansayed70@gmail.com>
2026-01-12 14:48:01 +0200

    doubly linked-list common features

---
+++ src/linear/linked_list/impl/DoublyLinkedListImpl.java
@@ -0,0 +1,112 @@
+package linear.linked_list.impl;
+
+import linear.linked_list.abstracts.AbstractLinkedList;
+import linear.linked_list.node.DoublyNode;
+
+public class DoublyLinkedListImpl<T> extends AbstractLinkedList<T, DoublyNode<T>> {
+    @Override
+    protected DoublyNode<T> getNodeAt(int index) {
+        checkIndex(index);
+
+        DoublyNode<T> current;
+        if (index < size / 2) {
+            // traverse from start
+            current = head;
+            for (int i = 0; i < index; i++)
+                current = current.getNext();
+        }
+        else {
+            // traverse from end
+            current = tail;
+            for (int i = size - 1; i > index; i--)
+                current = current.getPrev();
+        }
+
+        return current;
+    }
+
+    @Override
+    protected DoublyNode<T> getNextNode(DoublyNode<T> node) {
+        return node.getNext();
+    }
+
+    @Override
+    public void addFirst(T element) {
+        DoublyNode<T> newNode = new DoublyNode<>(element);
+
+        if (size == 0)
+            head = tail = newNode;
+
+        else {
+            newNode.setNext(head);
+            head.setPrev(newNode);
+            head = newNode;
+        }
+
+        size++;
+    }
+
+    @Override
+    public void addLast(T element) {
+        DoublyNode<T> newNode = new DoublyNode<>(element);
+        if (size == 0)
+            head = tail = newNode;
+
+        else {
+            tail.setNext(newNode);
+            newNode.setPrev(tail);
+            tail = newNode;
+        }
+
+        size++;
+    }
+
+    @Override
+    public T removeFirst() {
+        if (size == 0)
+            throw new IllegalStateException("Linked list is empty, no element to remove");
+
+        DoublyNode<T> deletedHead = head;
+        if (size == 1)
+            head = tail = null;
+
+        else {
+            head = deletedHead.getNext();
+            deletedHead.setNext(null);
+            head.setPrev(null);
+        }
+
+        size--;
+        return deletedHead.getData();
+    }
+
+    @Override
+    public T removeLast() {
+        if (size == 0)
+            throw new IllegalStateException("Linked list is empty, no element to remove");
+
+        DoublyNode<T> deletedTail = tail;
+
+        if (size == 1)
+            head = tail = null;
+
+        else {
+            tail = tail.getPrev();
+            tail.setNext(null);
+            deletedTail.setPrev(null);
+        }
+
+        size--;
+        return deletedTail.getData();
+    }
+
+    @Override
+    public T remove(int index) {
+        return null;
+    }
+
+    @Override
+    public void add(int index, T element) {
+
+    }
+}
