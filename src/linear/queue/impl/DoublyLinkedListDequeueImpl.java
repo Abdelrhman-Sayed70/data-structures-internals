0482985d0759b9be262556d2f14d8e534fdd6a5f
Abdelrhman Sayed <abdulrhmansayed70@gmail.com>
2026-01-13 16:51:50 +0200

    Implement DoublyLinkedList-based Dequeue with basic operations

--- src/linear/queue/impl/DoublyLinkedListDequeueImpl.java
+++ src/linear/queue/impl/DoublyLinkedListDequeueImpl.java
@@ -1,55 +1,76 @@
 package linear.queue.impl;
 
+import linear.linked_list.impl.DoublyLinkedListImpl;
 import linear.queue.api.Dequeue;
 
 public class DoublyLinkedListDequeueImpl<T> implements Dequeue<T> {
+    private final DoublyLinkedListImpl<T> linkedList;
+
+    public DoublyLinkedListDequeueImpl() {
+        linkedList = new DoublyLinkedListImpl<>();
+    }
+
     @Override
     public void pushBack(T element) {
-
+        linkedList.addLast(element);
     }
 
     @Override
     public void pushFront(T element) {
-
+        linkedList.addFirst(element);
     }
 
     @Override
     public T popBack() {
-        return null;
+        if (linkedList.isEmpty())
+            throw new IllegalStateException("Dequeue is empty");
+
+        return linkedList.removeLast();
     }
 
     @Override
     public T popFront() {
-        return null;
+        if (linkedList.isEmpty())
+            throw new IllegalStateException("Dequeue is empty");
+
+        return linkedList.removeFirst();
     }
 
     @Override
     public T back() {
-        return null;
+        if (linkedList.isEmpty())
+            throw new IllegalStateException("Dequeue is empty");
+
+        return linkedList.getTailValue();
     }
 
     @Override
     public T front() {
-        return null;
+        if (linkedList.isEmpty())
+            throw new IllegalStateException("Dequeue is empty");
+
+        return linkedList.getHeadValue();
     }
 
     @Override
     public int size() {
-        return 0;
+        return linkedList.size();
     }
 
     @Override
     public void clear() {
-
+        linkedList.clear();
     }
 
     @Override
     public boolean contains(T element) {
-        return false;
+        return linkedList.contains(element);
     }
 
     @Override
     public void print() {
-
+        System.out.println("Front");
+        linkedList.print();
+        System.out.println("Rear");
     }
 }
